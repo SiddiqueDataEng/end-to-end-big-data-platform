1d2bcf1d663c66643d487a23ec1612c04ff0fa49
Siddique Data Engineer <dba.siddique@gmail.com>
2016-11-25 13:52:00 +0500

    Implement data processing pipeline

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+64 End To End Big Data Platform - Main Implementation
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting 64-end-to-end-big-data-platform")
+    # Implementation here
+    pass
+
+if __name__ == "__main__":
+    main()
